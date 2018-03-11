acf2a30f1a7cd353b732dee3626455e797c49b7d
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-03-12 04:38:31 +0900

    add main.cpp

---
+++ deadreckoning/src/main.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/PoseStamped.h>
+
+const float M_PI = 3.1415926535897932;		// 円周率
+
+const float ENCODER_RESOLUTION = 500.0f;	// [P/R]
+const float OMNIWHEEL_DIAMETER = 0.040f;	// [m]
+
+const float DISTANCE_LRF_TO_CENTER = 0.00;	//[m]
+
+ros::Publisher pub;
+geometry_msgs::PoseStamped pose_msg;
+
+float yaw = 0.0f;
+float yawfirst = 0.0f;
+int old_X = 0;
+int old_y = 0;
+
+void encXCallback()
