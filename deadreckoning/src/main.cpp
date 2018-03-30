e0f8d9f64a5c6e85ae8500b858db1b8ac47d26fd
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-03-31 05:36:24 +0900

    Update

--- deadreckoning/src/main.cpp
+++ deadreckoning/src/main.cpp
@@ -8,6 +8,7 @@ const float DISTANCE_LRF_TO_CENTER = 0.00;	//[m]
 
 ros::Publisher pub;
 geometry_msgs::PoseStamped pose_msg;
+deadreckoning::enc& enc_msg;
 
 float yaw = 0.0f;
 float yawfirst = 0.0f;
@@ -16,10 +17,10 @@ float old_y = 0;
 
 void encCallback(const deadreckoning::enc& msg)
 {
-	pose_msg.pose.position.x += (msg.distance_X - old_x) * cos(yaw);
-	pose_msg.pose.position.y += (msg.distance_X - old_x) * sin(yaw);
-	pose_msg.pose.position.x += (msg.distance_Y - old_y) * sin(M_PI/2 + yaw);
-	pose_msg.pose.position.y += (msg.distance_Y - old_y) * cos(M_PI/2 + yaw);
+	pose_msg.pose.position.x += -(msg.distance_X - old_x) * cos(yaw);
+	pose_msg.pose.position.y += -(msg.distance_X - old_x) * sin(yaw);
+	pose_msg.pose.position.x += -(msg.distance_Y - old_y) * cos(M_PI/2 + yaw);
+	pose_msg.pose.position.y += -(msg.distance_Y - old_y) * sin(M_PI/2 + yaw);
 	old_x = msg.distance_X;
 	old_y = msg.distance_Y;
 	pub.publish(pose_msg);
