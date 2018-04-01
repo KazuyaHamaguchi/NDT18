fb7863eb47b9bc5e83d50811586e4dd83c3c9a33
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-02 03:21:48 +0900

    fix

--- accel_decel/src/accel_decel.cpp
+++ accel_decel/src/accel_decel.cpp
@@ -125,7 +125,7 @@ int main(int argc, char **argv)
 				if((t1 + t2) <= t && t <= (t1 + t2 + t3))
 				{
 					//ROS_INFO("time: %f\t V: %f\t X3", t, decel(t - (t1 + t2));
-					printf("%f\t %f\n", t, decel(t - ((t1 + t2) + abs(t1-t3))));
+					printf("%f\t %f\n", t, decel(t - ((t1 + t2) + (t3-t1))));
 				}
 			}
 
