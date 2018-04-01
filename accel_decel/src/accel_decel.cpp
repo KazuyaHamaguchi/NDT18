f73926705cc77b7119b81dc2e6c9e45d5b2e3f54
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-01 16:19:00 +0900

    fix

--- accel_decel/src/accel_decel.cpp
+++ accel_decel/src/accel_decel.cpp
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 
 		if(first)
 		{
-			while(t < /*(*/t1 /*+ t2 + t3)*/)
+			while(t <= (t1 + t2 /*+ t3)*/)
 			{
 				t += (current_time - last_time).toSec();
 
@@ -101,11 +101,11 @@ int main(int argc, char **argv)
 				{
 					ROS_INFO("time: %f\t V: %f", t, accel(t));
 				}
-				/*if(t1 <= t && t < t2)
+				if(t1 <= t && t <= t2)
 				{
 					ROS_INFO("time: %f\t V: %f", t, Vmax);
 				}
-				if(t2 <= t && t < t3)
+				/*if(t2 <= t && t < t3)
 				{
 					ROS_INFO("time: %f\t V: %f", t, decel(t));
 				}*/
