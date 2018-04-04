48d8d54cffb6f247f0177bc7504ae7247cabfb4f
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-04 20:32:17 +0900

    fix

--- nemcon/src/servo_test.cpp
+++ nemcon/src/servo_test.cpp
@@ -9,12 +9,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "servo_test");
 	ros::NodeHandle nh;
 
-	set_mode(pi, 22, PI_OUTPUT);
+	set_mode(pi, 18, PI_OUTPUT);
 	while(ros::ok())
 	{
-		set_servo_pulsewidth(pi, 22, 0);
+		set_servo_pulsewidth(pi, 18, 0);
 		ros::Duration(2).sleep();
-		set_servo_pulsewidth(pi, 22, 2000);
+		set_servo_pulsewidth(pi, 18, 2000);
 		ros::Duration(2).sleep();
 		ros::spinOnce();
 	}
