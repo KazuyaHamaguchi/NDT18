4e4f4352dcc25f45daa349af549d8deda40bde67
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-04 20:49:13 +0900

    Update

--- nemcon/src/servo_test.cpp
+++ nemcon/src/servo_test.cpp
@@ -10,13 +10,17 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 
 	set_mode(pi, 24, PI_OUTPUT);
+
 	while(ros::ok())
 	{
-		set_servo_pulsewidth(pi, 24, 0);
+		set_PWM_frequency(24, 400);
+		set_PWM_range(24, 2500);
+
+		/*set_servo_pulsewidth(pi, 24, 0);
 		ros::Duration(2).sleep();
 		set_servo_pulsewidth(pi, 24, 2000);
 		ros::Duration(2).sleep();
-		ros::spinOnce();
+		ros::spinOnce();*/
 	}
 	return 0;
 }
