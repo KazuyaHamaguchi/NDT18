246daecfcc7659e54150d580a036b803f496588f
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-05 07:17:25 +0900

    Update

--- nemcon/src/emergency_stop.cpp
+++ nemcon/src/emergency_stop.cpp
@@ -1,28 +1,29 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-#include <nemcon/motor.h>
+#include <mpu9250/motor.h>
 
 #include <pigpiod_if2.h>
 
 int pi = pigpio_start(0, 0);
 
 ros::Publisher pub;
-nemcon::motor msg_m;
+mpu9250::motor msg_m;
 
-#define e_pin 15
-int motor, accel;
-int flag = 0;
+#define e_pin 22
+bool motor = false;
+bool accel = false;
+bool flag = false;
 
-void callback_m(const nemcon::motor& msg)
+void callback_m(const mpu9250::motor& msg)
 {
 	if(msg.motor_FR == 0 && msg.motor_FL == 0 && msg.motor_RL == 0 && msg.motor_RR == 0)
 	{
 		ROS_INFO("M_OK");
-		motor = 1;
+		motor = true;
 	}
 	else
 	{
-		motor = 0;
+		motor = false;
 	}
 }
 
@@ -31,11 +32,11 @@ void callback_a(const sensor_msgs::Imu& msg)
 	if(msg.linear_acceleration.x > 2 || msg.linear_acceleration.x < -2 || msg.linear_acceleration.y > 2 || msg.linear_acceleration.y < -2)
 	{
 		ROS_INFO("A_OK");
-		accel = 1;
+		accel = true;
 	}
 	else
 	{
-		accel = 0;
+		accel = false;
 	}
 }
 
@@ -45,16 +46,16 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	ros::Subscriber sub_motor = nh.subscribe("motor", 1000, callback_m);
 	ros::Subscriber sub_imu = nh.subscribe("/imu/data_raw", 1000, callback_a);
-	pub = nh.advertise<nemcon::motor>("motor", 1000);
+	pub = nh.advertise<mpu9250::motor>("motor", 1000);
 	set_mode(pi, e_pin, PI_OUTPUT);
 
 	while(ros::ok())
 	{
-		if(motor == 1 && accel == 1)
+		if(motor && accel)
 		{
-			if(flag == 0)
+			if(!flag)
 			{
-				flag = 1;
+				flag = true;
 
 				gpio_write(pi, e_pin, 1);
 
@@ -71,9 +72,9 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			if(flag == 1)
+			if(flag)
 			{
-				flag = 0;
+				flag = false;
 				gpio_write(pi, e_pin, 0);
 				ROS_INFO("NG");
 			}
