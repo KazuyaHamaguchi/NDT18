ec37adb39f6301eef859d560e71622547c9dda58
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-04 22:20:00 +0900

    Update

--- nemcon/src/servo_test.cpp
+++ nemcon/src/servo_test.cpp
@@ -2,20 +2,22 @@
 
 #include <pigpiod_if2.h>
 
-int pi = pigpio_start(0, 0);
-
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "servo_test");
 	ros::NodeHandle nh;
-
+	int pi = pigpio_start(0, 0);
 	set_mode(pi, 24, PI_OUTPUT);
-	set_servo_pulsewidth(pi, 24, 1495);
-	ros::Rate loop_rate(1);
+	set_servo_pulsewidth(pi, 24, 1495);	//0度
+	//ros::Rate loop_rate(1);
 
 
 	while(ros::ok())
 	{
+		set_servo_pulsewidth(pi, 24, 1495);
+		time_sleep(1);
+		set_servo_pulsewidth(pi, 24, 540);
+		time_sleep(1);
 		/*set_PWM_frequency(pi, 24, 400);
 		set_PWM_range(pi, 24, 0);
 
@@ -24,7 +26,7 @@ int main(int argc, char **argv)
 		/*ros::Duration(2).sleep();
 		set_servo_pulsewidth(pi, 24, 2000);
 		ros::Duration(2).sleep();*/
-		loop_rate.sleep();
+		//loop_rate.sleep();
 		ros::spinOnce();
 	}
 	return 0;
