5fd487656c0c3ad00aba0518c77a631a45d6ddbe
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-05 10:19:49 +0900

    Comp

--- nemcon/src/servo_test.cpp
+++ nemcon/src/servo_test.cpp
@@ -9,27 +9,14 @@ int main(int argc, char **argv)
 	int pi = pigpio_start(0, 0);
 	set_mode(pi, 24, PI_OUTPUT);
 	set_servo_pulsewidth(pi, 24, 1495);	//0度
-	//ros::Rate loop_rate(1);
 
 
 	while(ros::ok())
 	{
 		set_servo_pulsewidth(pi, 24, 2450);
-		//time_sleep(1);
 		ros::Duration(1).sleep();
 		set_servo_pulsewidth(pi, 24, 650);
 		ros::Duration(1).sleep();
-		//time_sleep(1);
-		/*set_PWM_frequency(pi, 24, 400);
-		set_PWM_range(pi, 24, 0);
-
-		set_PWM_dutycycle(pi, 24, 500);*/
-
-		/*ros::Duration(2).sleep();
-		set_servo_pulsewidth(pi, 24, 2000);
-		ros::Duration(2).sleep();*/
-		//loop_rate.sleep();
-		//ros::spinOnce();
 	}
 	return 0;
 }
