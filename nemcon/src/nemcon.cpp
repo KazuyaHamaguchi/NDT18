5c3b4a8fd071856ebbe6a65de9783028dde03828
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-05 17:09:32 +0900

    test

--- nemcon/src/nemcon.cpp
+++ nemcon/src/nemcon.cpp
@@ -1,26 +1,29 @@
 #include <ros/ros.h>
 #include <nemcon/switch_in.h>
+#include <nemcon/tar_dis.h>
+#include <accel_decel/param.h>
 
 #include <pigpiod_if2.h>
 
 static const int pin_blue = 16;
-static const int pin_orange = 12;
+static const int pin_yellow = 12;
 static const int pin_servo = 24;
 
 int pi = pigpio_start(0, 0);
 
+void led_flash(int num, float time, int color);	//color：blue = 0, yellow = 1
+void switch_cb(const nemcon::switch_in& msg)
+
 void switch_cb(const nemcon::switch_in& msg)
 {
-	if(msg.START && msg.TZ1 && msg.TZ2 && msg.TZ3 && !msg.SC)
+	if(msg.START && !msg.START && msg.TZ1 && !msg.TZ2 && !msg.TZ3 && !msg.SC)
 	{
-		gpio_write(pi, pin_blue, 1);
-		gpio_write(pi, pin_orange, 1);
-		ROS_INFO("OK");
+		led_flash(3, 0.5, 1);
 	}
 	else
 	{
 		gpio_write(pi, pin_blue, 0);
-		gpio_write(pi, pin_orange, 0);
+		gpio_write(pi, pin_yellow, 0);
 		ROS_INFO("NO");
 	}
 }
@@ -47,4 +50,25 @@ int main(int argc, char **argv)
 	}
 	return 0;*/
 	ros::spin();
+}
+
+void led_flash(int num, float time, int color)
+{
+	for(int i = 0; i < num; i++)
+	{
+		if(color == 0)
+		{
+			gpio_write(pi, pin_blue, 1);
+			ros::Duration(time).sleep();
+			gpio_write(pi, pin_blue, 0);
+			ros::Duration(time).sleep();
+		}
+		if(color == 0)
+		{
+			gpio_write(pi, pin_yellow, 1);
+			ros::Duration(time).sleep();
+			gpio_write(pi, pin_yellow, 0);
+			ros::Duration(time).sleep();
+		}
+	}
 }
