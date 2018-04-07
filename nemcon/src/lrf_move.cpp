80248e69b574c83c449e158db252c5bd50769aa8
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-07 18:23:49 +0900

    Update lrf_move.cpp

--- nemcon/src/lrf_move.cpp
+++ nemcon/src/lrf_move.cpp
@@ -2,10 +2,12 @@
 #include <nemcon/pid_param.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <accel_decel/result.h>
+#include <nemcon/lrf_flag.h>
 
 void lrf_cb(const geometry_msgs::PoseStamped& msg);
+void flag_cb(const nemcon::lrf_flag& msg);
 
-bool flag = true;
+bool flag = false;
 bool flag_x = false;
 bool flag_y = false;
 bool flag_z = false;
@@ -13,6 +15,8 @@ bool flag_z = false;
 float lrf_x = 0.0f;
 float lrf_y = 0.0f;
 float lrf_z = 0.0f;
+
+float offsset = 0.0f;
 
 accel_decel::result msg_acc;
 nemcon::pid_param msg_pid_param;
@@ -27,99 +31,88 @@ int main(int argc, char **argv)
 	ros::Rate loop_rate(40);
 
 	ros::Subscriber sub_lrf = nh.subscribe("/lrf_pose", 1000, lrf_cb);
+	ros::Subscriber sub_flag = nh.subscribe("lrf_flag", 1000, flag_cb);
 
 	pub_tar_dis = nh.advertise<nemcon::pid_param>("pid_param", 1000);
 	pub_acc = nh.advertise<accel_decel::result>("accel_decel/result", 1000);
 
 	msg_pid_param.pattern = 1;
-  msg_pid_param.speed = 1;
+	msg_pid_param.speed = 1;
 	msg_acc.Vmax = false;
-	flag = true;
 
 	while(ros::ok())
 	{
-			if(flag)
+		if(flag)
+		{
+			if(lrf_x > 0.01 + offsset && !flag_x)
 			{
-				if(lrf_x > 0.01 && !flag_x)
-				{
-					ROS_INFO("lrf_x:%f", lrf_x);
-					msg_acc.V = 0.05;
-					msg_pid_param.front = 4;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_x = false;
-				}
-				if(lrf_x < -0.01 && !flag_x)
-				{
-					ROS_INFO("-lrf_x:%f", lrf_x);
-					msg_acc.V = 0.05;
-					msg_pid_param.front = 2;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_x = false;
-				}
-				if(-0.01 <= lrf_x && lrf_x <= 0.01)
-				{
-					ROS_INFO("lrf_x OK");
-					msg_acc.V = 0;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_x = true;
-				}
-        else
-        {
-          flag_x = false;
-        }
+				ROS_INFO("lrf_x:%f", lrf_x);
+				msg_acc.V = 0.05;
+				msg_pid_param.front = 4;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_x = false;
 			}
-
-			if(flag_x)
+			if(lrf_x < -0.01 + offsset && !flag_x)
 			{
-				if(lrf_y > 0 && !flag_y)
-				{
-					ROS_INFO("lrf_y:%f", lrf_y);
-					msg_acc.V = 0.05;
-					msg_pid_param.front = 3;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_y = false;
-				}
-				if(lrf_y < 0 && flag_x && !flag_y)
-				{
-					ROS_INFO("-lrf_y:%f", lrf_y);
-					msg_acc.V = 0.05;
-					msg_pid_param.front = 1;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_y = false;
-				}
-				if(-0.01 < lrf_y && lrf_y < 0.01)
-				{
-					ROS_INFO("lrf_y OK");
-					msg_acc.V = 0;
-					pub_tar_dis.publish(msg_pid_param);
-					pub_acc.publish(msg_acc);
-					flag_y = true;
-				}
-        else
-        {
-          flag_y = false;
-        }
+				ROS_INFO("-lrf_x:%f", lrf_x);
+				msg_acc.V = 0.05;
+				msg_pid_param.front = 2;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_x = false;
 			}
+			if(-0.01 + offsset <= lrf_x && lrf_x <= 0.01 + offsset)
+			{
+				ROS_INFO("lrf_x OK");
+				msg_acc.V = 0;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_x = true;
+			}
+			else
+			{
+			  flag_x = false;
+			}
+		}
 
-      /*if(!flag_z)
-      {
-        if((lrf_z > 0.001 || lrf_z < -0.001) && !flag_z)
-        {
-          ROS_INFO("lrf_z:%f", lrf_z);
-          msg_pid_param.speed = -1;
-          pub_tar_dis.publish(msg_pid_param);
-          flag_z = false;
-        }
-        else
-        {
-          ROS_INFO("lrf_z OK!");
-          msg_pid_param.speed = 0;
-        }*/
+		if(flag_x)
+		{
+			if(lrf_y > 0 && !flag_y)
+			{
+				ROS_INFO("lrf_y:%f", lrf_y);
+				msg_acc.V = 0.05;
+				msg_pid_param.front = 3;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_y = false;
+			}
+			if(lrf_y < 0 && flag_x && !flag_y)
+			{
+				ROS_INFO("-lrf_y:%f", lrf_y);
+				msg_acc.V = 0.05;
+				msg_pid_param.front = 1;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_y = false;
+			}
+			if(-0.01 < lrf_y && lrf_y < 0.01)
+			{
+				ROS_INFO("lrf_y OK");
+				msg_acc.V = 0;
+				pub_tar_dis.publish(msg_pid_param);
+				pub_acc.publish(msg_acc);
+				flag_y = true;
+			}
+			else
+			{
+			  flag_y = false;
+			}
+		}
+		if(flag_x && flag_y)
+		{
+			flag = false;
+		}
 
 		loop_rate.sleep();
 		ros::spinOnce();
@@ -131,6 +124,25 @@ void lrf_cb(const geometry_msgs::PoseStamped& msg)
 	lrf_x = msg.pose.position.x;
 	lrf_y = msg.pose.position.y;
 	lrf_z = msg.pose.orientation.z;
+	//ROS_INFO("%f\t %f\t %f\t", lrf_x, lrf_y, lrf_z);
+}
 
-  //ROS_INFO("%f\t %f\t %f\t", lrf_x, lrf_y, lrf_z);
+void flag_cb(const nemcon::lrf_flag& msg)
+{
+	flag = msg.flag;
+
+	switch(msg.TZ)
+	{
+		case 1: case 3:
+			offsset = 0.0f;
+			break;
+
+		case 2:
+			offsset = 0.0f;
+			break;
+
+		default:
+			offsset = 0.0f;
+			break;
+	}
 }
