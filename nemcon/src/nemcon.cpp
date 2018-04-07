4b7642b746165288a2e80337d58371f0b06b81f0
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-04-07 12:20:19 +0900

    test

--- nemcon/src/nemcon.cpp
+++ nemcon/src/nemcon.cpp
@@ -17,9 +17,6 @@ bool enc_flag = false;
 bool end = false;
 
 float acc_t = 0.0f;
-float lrf_x = 0.0f;
-float lrf_y = 0.0f;
-float lrf_z = 0.0f;
 
 void acc_t_cb(const accel_decel::result& msg);
 
@@ -36,18 +33,20 @@ void switch_cb(const nemcon::switch_in& msg)
 {
 	if(msg.START)
 	{
-		if(msg.SZ && !msg.TZ1 && !msg.TZ2 && !msg.TZ3 && !msg.SC && !cb_flag)
+		if(!msg.SZ && !msg.TZ1 && !msg.TZ2 && !msg.TZ3 && !msg.SC && !cb_flag)
 		{
 			led_flash(0, 0, 2);
 			led_flash(3, 0.1, 0);
 			led_flash(-1, 0, 0);
 
 			acc_move(0, 1, 0, 0.5, 1.05, 0, 0, 4);	//SZ横
-			ros::Duration(3.6324493885 + 0.05).sleep();;
+			ros::spinOnce();
+			ROS_INFO("%f", acc_t);
+			/*ros::Duration(3.6324493885 + 0.05).sleep();;
 			acc_move(0, 1, 0, 0.5, 4.8, -1.1, 0, 1);	//TZ1横
 			ros::Duration(7.94159317017 + 0.05).sleep();
 			acc_move(0, 1, 0, 0.5, 1, -1.15, 4.5, 4);	//TZ1受け渡しポイント
-			ros::Duration(3.54490756989 + 0.05).sleep();
+			ros::Duration(3.54490756989 + 0.05).sleep();*/
 			//lrf_move(3);
 
 
