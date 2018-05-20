c94e42926ac60d1d62dfec1b092206e0bd5195a7
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-05-21 00:26:02 +0900

    fix

--- mpu9250/src/Gyro.cpp
+++ mpu9250/src/Gyro.cpp
@@ -82,6 +82,8 @@ int main(int argc, char **argv)
 	{
 		current_time = ros::Time::now();
 		dt = (current_time - last_time).toSec();
+		_
+		ROS_INFO("%f", dt);
 
 		i2c_read_i2c_block_data(pi, handle, 0x43, data, 6);
 		float rawX = gyroCoefficient * u2s(data[0] << 8 | data[1]);
