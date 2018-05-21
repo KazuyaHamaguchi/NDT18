61848f5dd352612a0a57fd9d478ead0a9c3a68de
KazuyaHamaguchi <kzhamachan@gmail.com>
2018-05-21 19:56:01 +0900

    Update main.cpp

--- deadreckoning/src/main.cpp
+++ deadreckoning/src/main.cpp
@@ -2,26 +2,47 @@
 #include <deadreckoning/enc.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <nemcon/switch_in.h>
 #include <math.h>
 
 const float DISTANCE_LRF_TO_CENTER = 0.00;	//[m]
 
 ros::Publisher pub;
 geometry_msgs::PoseStamped pose_msg;
+ros::Publisher pub_switch;
 
 float yaw = 0.0f;
 float yawfirst = 0.0f;
 float old_x = 0;
 float old_y = 0;
+bool RESET = false;
+
+void switch_cb(const nemcon::switch_in& msg)
+{
+	if(msg.RESET == true)
+	{
+		pose_msg.pose.position.x += (msg.distance_X - old_x) * cos(yaw);
+		pose_msg.pose.position.y += (msg.distance_X - old_x) * sin(yaw);
+		pose_msg.pose.position.x += (msg.distance_Y - old_y) * cos(M_PI/2 + yaw);
+		pose_msg.pose.position.y += (msg.distance_Y - old_y) * sin(M_PI/2 + yaw);
+		old_x = msg.distance_X;
+		old_y = msg.distance_Y;
+	}
+}
 
 void encCallback(const deadreckoning::enc& msg)
 {
-	pose_msg.pose.position.x += (msg.distance_X - old_x) * cos(yaw);
-	pose_msg.pose.position.y += (msg.distance_X - old_x) * sin(yaw);
-	pose_msg.pose.position.x += (msg.distance_Y - old_y) * cos(M_PI/2 + yaw);
-	pose_msg.pose.position.y += (msg.distance_Y - old_y) * sin(M_PI/2 + yaw);
-	old_x = msg.distance_X;
-	old_y = msg.distance_Y;
+	pose_msg.pose.position.x = 0;
+	pose_msg.pose.position.y = 0;
+	pose_msg.pose.position.x = 0;
+	pose_msg.pose.position.y = 0;
+	old_x = 0;
+	old_y = 0;
+	yaw = 0;
+	pose_msg.pose.orientation.w = 0;
+	pose_msg.pose.orientation.x = 0;
+	pose_msg.pose.orientation.y = 0;
+	pose_msg.pose.orientation.z = 0;
 	pub.publish(pose_msg);
 }
 
@@ -43,6 +64,7 @@ int main(int argc, char **argv)
 
 	ros::Subscriber subEnc = nh.subscribe("/enc", 1000, encCallback);
 	ros::Subscriber subIMU = nh.subscribe("/imu/data_raw", 1000, imuCallback);
+	ros::Subscriber sub_Switch = nh.subscribe("switch", 1000, switch_cb);
 
 	pose_msg.header.frame_id = "/map";
 	pose_msg.pose.position.x = pose_msg.pose.position.y = pose_msg.pose.position.z = 0.0f;
